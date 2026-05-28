21ce2f54f4166cce8a865f30745c0aabed175206
mb-harikrishnan <mb.harikrishnan@gmail.com>
2026-05-28 15:46:48 +0530

    f

--- little_heart_admin/application/config/routes.php
+++ little_heart_admin/application/config/routes.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------------
@@ -55,3 +55,4 @@ $route['check_username'] = 'welcome/check_username';
 
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+//paranaari abhi 
