5d1c02aa0cbc336a62da72f40b92fbbe1af1e5bc
shabeeb <mohmammadshabeeb500@gmail.com>
2026-09-21 22:46:15 +0530

    ghadvhgsdvf

--- school_admin/application/config/routes.php
+++ school_admin/application/config/routes.php
@@ -314,6 +314,10 @@ $route['save']                   = 'members_area/PermissionsController/save';
 $route['teacherdashboard'] = 'members_area/TeacherController/teacherdashboard';
 
 
+
+$route['parapattiiiiiiiii'] = 'members_area/TeacherController/pariiiiiiiiiiiiiiiiiii';
+
+
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
